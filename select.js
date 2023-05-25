926c74386494694c145799c6725c014f517f38e3
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-05-25 16:57:24 -0300

    Commit 25052023-1657

--- select.js
+++ select.js
@@ -1,7 +1,13 @@
 const selectArray = ['Ótimo', 'Bom', 'Regular', 'Ruim', 'Péssimo']
 
 let select = selectArray.map(function(option){
-    return `<option value="${option}">${option}</option>`;
+    return `
+            <div id="option" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <option value="${option}">${option}</option>
+                </div>
+            </div>
+            `;
 })
 
 document.querySelector('#nota').innerHTML = select.join("");
